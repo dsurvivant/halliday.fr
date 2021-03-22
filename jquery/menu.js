f48d5ca9a249c5204becf5486167eaf48abbd365
Jean Michel TENTELIER <jmtentelier@gmail.com>
2021-03-22 17:38:07 +0100

    travail barre de navigation

--- jquery/menu.js
+++ jquery/menu.js
@@ -3,15 +3,7 @@
 /**                                          Programmeur: Michemuche62        
     script de gestion de la barre de menu
 
-    - PROCEDURES:
-        * 1 - Changement de résolution d'écran : 
-            ==> en résolution mobile le menu disparait (menu  en position verticale)
-            ==> hors résolution mobile le menu s'affiche s'il est effacé (menu en position horizontale)
-
-        * 2 - Bouton menu glissant :
-            ==> effectue le glissement du menu glissant à chaque appui sur le bouton
-
-        * 3 -CONNEXION UTILISATEUR: 
+        *  -CONNEXION UTILISATEUR: 
             ==> vérification des champs
             ==> si non valide, on reste sur la page et on affiche les messages d'erreurs
             ==> si valide soumission(submit) du formulaire
@@ -23,35 +15,9 @@ $(function()
     var decennie; //decennie déterminé par clic dans le menu
     var memodecennie=$('#liste_decennie li:first()').attr('class'); //memorisation de la decennie avant changement suite à clic sur menu
     var taille_ecran
-
-    //on cache le menu en résolution mobile
-    taille_ecran = $(window).width();
-    if (taille_ecran  < 768)
-        {
-            $('#barre_navigation').hide();
-            $('#barre_navigation').css('visibility','visible');
-        }
-
-    //******************
-    // 1 - changement de résolution de l'écran
-    //******************
-        $(window).resize(function()
-        {
-            taille_ecran = $(window).width();
-            if (taille_ecran  >768) { $('#barre_navigation').show(); }  
-        });
-    //******************
-    // 2 - bouton menu glissant
-    //******************
-
-        //menu glissant pour la résolution mobile
-            $('#image_menu').click(function()
-            {
-                $('#barre_navigation').toggle('slide', { direction: 'right' }, 500);        
-            });
     
     //***************** 
-    //  3 - menu de connexion
+    //  menu de connexion
     //*****************
 
         // soumission du formulaire de connexion controle des champs de saisi
--- pages/site/views/view_barre_navigation.php
+++ pages/site/views/view_barre_navigation.php
@@ -14,55 +14,84 @@
 		<button class="navbar-toggler" data-toggle="collapse" data-target="#menu">
            <span class="navbar-toggler-icon"></span>
         </button>
-	<!-- LIENS -->
+	
+	<!-- CONTENU RESPONSIVE --> 
 		<div id="menu" class="collapse navbar-collapse">
-			<ul class="navbar-nav">
+			<!-- LIENS -->
+				<ul class="navbar-nav mr-auto">
+				<!-- CHOIX MENU ACCUEIL -->
+					<li class="nav-item"> 
+						<a class="nav-link" href="index.php"> Accueil</a> 
+					</li>
+				<!-- CHOIX MENU DISCOGRAPHIE -->
+		        	<li  class="nav-item dropdown">
+		            	<a id="menuDiscographie" class="nav-link dropdown-toggle"  href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Discographie
+		            	</a>
 
-			<!-- CHOIX MENU ACCUEIL -->
-				<li class="nav-item active"> 
-					<a class="nav-link" href="index.php"> Accueil</a> 
-				</li>
-			<!-- CHOIX MENU DISCOGRAPHIE -->
-	        	<li  class="nav-item dropdown">
-	            	<a id="menuDiscographie" class="nav-link dropdown-toggle"  href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Discographie
-	            	</a>
+						<div id="liste_decennie" class="dropdown-menu">
+							<a class="dropdown-item" href="index.php?decennie=1960"> 1960 - 1969</a>
+							<a class="dropdown-item" href="index.php?decennie=1970"> 1970 - 1979</a>
+							<a class="dropdown-item" href="index.php?decennie=1980"> 1980 - 1989</a>
+							<a class="dropdown-item" href="index.php?decennie=1990"> 1990 - 1999</a>
+							<a class="dropdown-item" href="index.php?decennie=2000"> 2000 - 2009</a>
+							<a class="dropdown-item" href="index.php?decennie=2010"> 2010 - 2018</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="index.php?decennie=tous"> Tous</span></a>
+						</div>
+		        	</li>
+		        <!-- CHOIX MENU Fiches Albums-->
+		           <li  class="nav-item dropdown">
+		            	<a id="menuFichesAlbums" class="nav-link dropdown-toggle"  href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Fiches Albums
+		            	</a>
 
-					<div id="liste_decennie" class="dropdown-menu">
-						<a class="dropdown-item" href="index.php?decennie=1960"> 1960 - 1969</a>
-						<a class="dropdown-item" href="index.php?decennie=1970"> 1970 - 1979</a>
-						<a class="dropdown-item" href="index.php?decennie=1980"> 1980 - 1989</a>
-						<a class="dropdown-item" href="index.php?decennie=1990"> 1990 - 1999</a>
-						<a class="dropdown-item" href="index.php?decennie=2000"> 2000 - 2009</a>
-						<a class="dropdown-item" href="index.php?decennie=2010"> 2010 - 2018</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="index.php?decennie=tous"> Tous</span></a>
-					</div>
-	        	</li>
-	        <!-- CHOIX MENU Fiches Albums-->
-	           <li  class="nav-item dropdown">
-	            	<a id="menuDiscographie" class="nav-link dropdown-toggle"  href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Fiches Albums
-	            	</a>
+						<div id="liste_fiches_albums" class="dropdown-menu">
+							<a class="dropdown-item" href="index.php?fiche=1960-hello_johnny"> 1960 - Hello Johnny</a>
+							<a class="dropdown-item" href="index.php?fiche=1961-nous_les_gars_nous_les_filles"> 1961 - Nous les gars, nous les filles</a>
+							<a class="dropdown-item" href="index.php?fiche=1961-tete_a_tete_avec_johnny"> 1961 - Tête à tête avec Johnny</a>
+							<a class="dropdown-item" href="index.php?fiche=1961-viens_danser_le_twist"> 1961 - Viens danser le twist</a>
+							<a class="dropdown-item" href="index.php?fiche=1961-salut_les_copains"> 1961 - Salut les copains!</a>
+							<a class="dropdown-item" href="index.php?fiche=1962-sings_america_s_rock_in_hits"> 1962- Sings America's Rock'in'hits</a>
+							<a class="dropdown-item" href="index.php?fiche=1962-retiens_la_nuit"> 1962- Retiens la nuit</a>
+							<a class="dropdown-item" href="index.php?fiche=1968-jeune_homme"> 1968 - Jeune Homme</a>
+							<a class="dropdown-item" href="index.php?fiche=1968-reve_et_amour"> 1968 - Rêve et amour</a>
+							<a class="dropdown-item" href="index.php?fiche=1970-vie"> 1970 - Vie</a>
+							<a class="dropdown-item" href="index.php?fiche=1971-flagrant_delit"> 1971 - Flagrant Délit</a>
+							<a class="dropdown-item" href="index.php?fiche=1973-insolitudes"> 1973 - Insolitudes</a>
+							<a class="dropdown-item" href="index.php?fiche=1974-rockandslow"> 1974 - Rock'n'slow</a>
+							<a class="dropdown-item" href="index.php?fiche=1976-derriere_l_amour"> 1976 - Derrière L'amour</a>
+							<a class="dropdown-item" href="index.php?fiche=1976-hamlet"> 1976 - Hamlet</a>
+							<a class="dropdown-item" href="index.php?fiche=1977-c'est_la_vie"> 1977 - C'est la vie</a>
+							<a class="dropdown-item" href="index.php?fiche=1978-solitudes_a_deux"> 1978 - Solitudes à deux</a>
+							<a class="dropdown-item" href="index.php?fiche=1979-hollywood"> 1979 - Hollywood</a>
+							<a class="dropdown-item" href="index.php?fiche=1980-a_partir_de_maintenant"> 1980 - A partir de maintenant</a>
+							<a class="dropdown-item" href="index.php?fiche=1981-en_pieces_detachees"> 1981 - En pièces détachées</a>
+							<a class="dropdown-item" href="index.php?fiche=1981-pas_facile"> 1981 - Pas facile</a>
+							<a class="dropdown-item" href="index.php?fiche=1982-quelque_part_un_aigle"> 1982 - Quelque part un aigle</a>
+							<a class="dropdown-item" href="index.php?fiche=1982-la_peur"> 1982 - La peur</a>
+							<a class="dropdown-item" href="index.php?fiche=1989-cadillac"> 1989 - Cadillac</a>
+							<a class="dropdown-item" href="index.php?fiche=2007-le_coeur_d_un_homme"> 2007 - Le coeur d'un homme</a>
+							<a class="dropdown-item" href="index.php?fiche=2008-ca_ne_finira_jamais"> 2008 - Ca ne finira jamais</a> 
+						
+						</div>
+		        	</li>
+		         </ul>
 
-					<div id="liste_decennie" class="dropdown-menu">
-						<a class="dropdown-item" href="index.php?decennie=1960"> 1960 - 1969</a>
-						<a class="dropdown-item" href="index.php?decennie=1970"> 1970 - 1979</a>
-						<a class="dropdown-item" href="index.php?decennie=1980"> 1980 - 1989</a>
-						<a class="dropdown-item" href="index.php?decennie=1990"> 1990 - 1999</a>
-						<a class="dropdown-item" href="index.php?decennie=2000"> 2000 - 2009</a>
-						<a class="dropdown-item" href="index.php?decennie=2010"> 2010 - 2018</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="index.php?decennie=tous"> Tous</span></a>
-					</div>
-	        	</li>
-	         </ul>
+		    <!-- FORMULAIRE DE RECHERCHE -->
+				<form class="form-inline">
+			         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			      <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Recherche</button>       
+			    </form>
+			<!-- BOUTON DE CONNEXION -->
+
+				<div class="text-right">
+					<a href="index.php?demandeconnexion">
+						<button class="btn btn-outline-info ml-1 my-2 my-sm-0">Connexion</button>
+					</a>
+				</div>
+
+
 		</div>
-	<!-- FORMULAIRE DE RECHERCHE -->
-		<!--
-		<form class="form-inline">
-	         <input class="form-control mr-sm-2" type="search" placeholder="Search">
-	      <button class="btn btn-primary" type="submit">Recherche</button>       
-	    </form>
-	-->
+	
 
 	<!-- FORMULAIRE DE CONNEXION -->
 </nav>
