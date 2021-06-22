5685a1c3f9ec006d1fa805e4aaf4b682e7a20628
dsurvivant <jmtentelier@gmail.com>
2021-06-22 14:49:57 +0200

    page maintenance

---
+++ maintenance.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Maintenance</title>
+
+	<style type="text/css">
+		body
+		{
+			background-color: lightgreen;
+			text-align: center;
+			font-size: 80px;
+			margin-top: 100px;
+		}
+
+	</style>
+
+</head>
+<body>
+	Maintenance en cours
+</body>
+</html>
